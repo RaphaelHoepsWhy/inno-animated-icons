ace40d7f5e9d2396a97a35d917807848c88d5564
RaphaelHoepsWhy <raphael.hoeps@why.de>
2024-10-02 11:06:26 +0200

    feat: AI

--- app/components/IconsFramerMotion/IconsFramerMotion.tsx
+++ app/components/IconsFramerMotion/IconsFramerMotion.tsx
@@ -6,30 +6,36 @@ import { variants, variantsTail } from "./paths"
 
 export const IconsFramerMotion = () => {
   return (
-    <motion.svg
-      xmlns="http://www.w3.org/2000/svg"
-      version="1.1"
-      width={"100px"}
-      height={"100px"}
-      viewBox="0 0 24 24"
-      initial="start"
-      whileHover={"hover"}
-    >
-      <motion.path
-        variants={variants[0]}
-        stroke={"white"}
-        strokeWidth={1.5}
-        strokeLinecap={"round"}
-        strokeLinejoin={"round"}
-      />
-      <motion.path
-        variants={variantsTail[0]}
-        stroke={"white"}
-        strokeWidth={1.5}
-        strokeLinecap={"round"}
-        strokeLinejoin={"round"}
-        fill={"transparent"}
-      />
-    </motion.svg>
+    <div className="flex gap-8">
+      {variants.map((variant, index) => (
+        <motion.svg
+          key={index}
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          width={"100px"}
+          height={"100px"}
+          viewBox="0 0 24 24"
+          initial="start"
+          whileHover={"hover"}
+        >
+          <motion.path
+            variants={variant}
+            stroke={"white"}
+            strokeWidth={1.5}
+            strokeLinecap={"round"}
+            strokeLinejoin={"round"}
+            fill={"transparent"}
+          />
+          <motion.path
+            variants={variantsTail[index]}
+            stroke={"white"}
+            strokeWidth={1.5}
+            strokeLinecap={"round"}
+            strokeLinejoin={"round"}
+            fill={"transparent"}
+          />
+        </motion.svg>
+      ))}
+    </div>
   )
 }
